72cd4870390d4f6ce33b88f99e3b223107550ee9
Adam <mohdadamfirdaus@graduate.utm.my>
2025-02-15 11:33:29 +0800

    part3 &part4

--- lib/history.dart
+++ lib/history.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-class HistoryScreen extends StatelessWidget {
-  final List<Map<String, String>> history;
+class HistoryScreen extends StatefulWidget {
+  @override
+  _HistoryScreenState createState() => _HistoryScreenState();
+}
 
-  HistoryScreen({required this.history, String? selectedType});
+class _HistoryScreenState extends State<HistoryScreen> {
+  List<Map<String, String>> history = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistory();
+  }
+
+  Future<void> _loadHistory() async {
+    var box = Hive.box('activityBox');
+    setState(() {
+      history = (box.get('history') as List?)
+              ?.map((e) => Map<String, String>.from(e))
+              .toList() ??
+          [];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('History')),
-      body: ListView.builder(
-        itemCount: history.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(history[index]['activity']!),
-            subtitle: Text(history[index]['price']!),
-          );
-        },
-      ),
+      appBar: AppBar(title: Text('History'),backgroundColor: Colors.green,),
+      body: history.isEmpty
+          ? Center(child: Text('No history available'))
+          : ListView.builder(
+              itemCount: history.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(history[index]['activity']!),
+                  subtitle: Text(history[index]['price']!),
+                );
+              },
+            ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hendshake_technical_assessment/history.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'history.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('activityBox');
   runApp(MyApp());
 }
 
@@ -11,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       debugShowCheckedModeBanner: false,
       title: 'Flutter Next Button',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -26,22 +31,52 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String activity = "Press 'Next' to get an activity";
+  String activity = "Press 'Next' to generate random activity";
   String price = "";
   List<Map<String, String>> history = [];
+  String? selectedType;
+  final List<String> activityTypes = [
+    'education', 'recreational', 'social', 'diy', 'charity',
+    'cooking', 'relaxation', 'music', 'busywork'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPreferences();
+  }
+
+  Future<void> _loadPreferences() async {
+    var box = Hive.box('activityBox');
+    setState(() {
+      selectedType = box.get('selectedType');
+      history = List<Map<String, String>>.from(box.get('history', defaultValue: []));
+    });
+  }
+
+  Future<void> _savePreferences() async {
+    var box = Hive.box('activityBox');
+    await box.put('selectedType', selectedType);
+    await box.put('history', history);
+  }
 
   Future<void> fetchActivity() async {
-    final response = await http.get(Uri.parse('https://bored.api.lewagon.com/api/activity'));
+    String url = 'https://bored.api.lewagon.com/api/activity';
+    if (selectedType != null && selectedType!.isNotEmpty) {
+      url = 'https://bored.api.lewagon.com/api/activity?type=${Uri.encodeComponent(selectedType!)}';
+    }
+    final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       setState(() {
         activity = data['activity'];
         price = "Price: \$${data['price'].toString()}";
-        history.insert(0, {'activity': activity, 'price': price});
+        history.insert(0, {'activity': activity, 'price': price, 'type': data['type']});
         if (history.length > 50) {
           history.removeLast();
         }
       });
+      _savePreferences();
     } else {
       setState(() {
         activity = 'Failed to load activity';
@@ -53,48 +88,78 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Main Screen')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text(
-                    activity,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    price,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ],
+      appBar: AppBar(title: Text('Technical Assessment'),backgroundColor: Colors.green,),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        color: Colors.grey[200],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButton<String>(
+                value: selectedType,
+                hint: Text('Select Activity Type'),
+                items: activityTypes.map((type) {
+                  return DropdownMenuItem(
+                    value: type,
+                    child: Text(type),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedType = value;
+                  });
+                  _savePreferences();
+                },
               ),
-            ),
-            ElevatedButton(
-              onPressed: fetchActivity,
-              child: Text('Next'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HistoryScreen(history: history)),
-                );
-              },
-              child: Text('History'),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16.0),
+                padding: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5.0,
+                      spreadRadius: 1.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      activity,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      price,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: fetchActivity,
+                child: Text('Next'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HistoryScreen()),
+                  );
+                },
+                child: Text('History'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
